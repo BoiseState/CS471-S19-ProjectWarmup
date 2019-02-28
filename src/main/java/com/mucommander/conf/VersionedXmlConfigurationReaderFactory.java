212583a0463ac9cdf338652558e5ac5ff3bf9787
Krist Veruari <KristVeruari@u.boisestate.edu>
2019-02-28 14:13:51 -0700

    Fixes #116

--- src/main/java/com/mucommander/conf/VersionedXmlConfigurationReaderFactory.java
+++ src/main/java/com/mucommander/conf/VersionedXmlConfigurationReaderFactory.java
@@ -15,9 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@BUG HighPriority - gomr (2 of 3) ->|
-@BUG HighPriority - gomr (2 of 3) ->|package com.mucommander.conf;
-@BUG HighPriority - gomr (2 of 3) ->|
+
+package com.mucommander.conf;
+
 import com.mucommander.commons.conf.ConfigurationReader;
 import com.mucommander.commons.conf.ConfigurationReaderFactory;
 import com.mucommander.commons.conf.ReaderConfigurationException;
