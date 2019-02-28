f953cadb7fe1c760343c0fa61fb920dda178c3ef
Bogdan Dit <bogdandit@boisestate.edu>
2019-02-27 17:33:30 -0700

    Fixes #6

--- src/main/java/com/mucommander/command/PermissionsFileFilter.java
+++ src/main/java/com/mucommander/command/PermissionsFileFilter.java
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@BUG MediumPriority - ynse (2 of 2) ->|
-@BUG MediumPriority - ynse (2 of 2) ->|package com.mucommander.command;
+
+package com.mucommander.command;
 
 import com.mucommander.commons.file.AbstractFile;
 import com.mucommander.commons.file.PermissionAccess;
