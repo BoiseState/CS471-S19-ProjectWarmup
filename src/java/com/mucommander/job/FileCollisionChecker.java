b5bfb6a3e7e5df43ae03b1c8362863eddab25d7e
jason smith <jasonsmith7@u.boisestate.edu>
2019-03-03 10:09:09 -0700

    Fixes #68

--- src/java/com/mucommander/job/FileCollisionChecker.java
+++ src/java/com/mucommander/job/FileCollisionChecker.java
@@ -15,9 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@BUG HighPriority - bejw (2 of 3) ->|
-@BUG HighPriority - bejw (2 of 3) ->|package com.mucommander.job;
-@BUG HighPriority - bejw (2 of 3) ->|
+package com.mucommander.job;
+
 import com.mucommander.commons.file.AbstractFile;
 
 /**
