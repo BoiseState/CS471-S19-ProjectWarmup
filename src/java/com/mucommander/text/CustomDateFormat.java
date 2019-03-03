946945955ca5a98ab2471023d61153e5c1e28709
Mike Blowers <mjblowers@gmail.com>
2019-03-03 11:10:31 -0700

    Fixes #100

--- src/java/com/mucommander/text/CustomDateFormat.java
+++ src/java/com/mucommander/text/CustomDateFormat.java
@@ -15,9 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@BUG HighPriority - plbh (2 of 3) ->|
-@BUG HighPriority - plbh (2 of 3) ->|package com.mucommander.text;
-@BUG HighPriority - plbh (2 of 3) ->|
+
+package com.mucommander.text;
+
 import com.mucommander.commons.conf.ConfigurationEvent;
 import com.mucommander.commons.conf.ConfigurationListener;
 import com.mucommander.conf.MuConfigurations;
