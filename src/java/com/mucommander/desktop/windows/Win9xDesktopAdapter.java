4a8a94a580f45be13450645963af6d523947470a
Justin Carpenter <justincarpenter836@u.boisestate.edu>
2019-03-03 21:18:42 -0700

    Fixes #105

--- src/java/com/mucommander/desktop/windows/Win9xDesktopAdapter.java
+++ src/java/com/mucommander/desktop/windows/Win9xDesktopAdapter.java
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@BUG MediumPriority - nfro (1 of 2) ->|
-@BUG MediumPriority - nfro (1 of 2) ->|package com.mucommander.desktop.windows;
+
+package com.mucommander.desktop.windows;
 
 import com.mucommander.command.Command;
 import com.mucommander.command.CommandException;
