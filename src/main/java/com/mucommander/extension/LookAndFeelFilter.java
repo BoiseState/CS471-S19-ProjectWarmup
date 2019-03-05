be675f75209d66700a49d3713ece120bab2cf5bc
jedthe3rd <tariandeath210@gmail.com>
2019-03-04 20:50:40 -0700

    Fixes #94

--- src/main/java/com/mucommander/extension/LookAndFeelFilter.java
+++ src/main/java/com/mucommander/extension/LookAndFeelFilter.java
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@BUG MediumPriority - odwj (2 of 2) ->|
-@BUG MediumPriority - odwj (2 of 2) ->|package com.mucommander.extension;
+
+package com.mucommander.extension;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
